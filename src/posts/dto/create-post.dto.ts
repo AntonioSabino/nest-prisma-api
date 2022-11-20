fe1d1fe063c1d8ac0fc9408efe8ecc2dcd12cc34
Antonio Sabino <aslimajr@outlook.com>
2022-11-20 12:48:41 -0300

    chore: add ApiProperty

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,14 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class CreatePostDto {
+  @ApiProperty({ description: 'The title of the post' })
   @IsString()
   @IsNotEmpty()
   title: string;
 
+  @ApiProperty({ description: 'The content of the post' })
   @IsString()
   @IsOptional()
   content?: string;
 
+  @ApiProperty({ description: 'The email of the author' })
   @IsEmail()
   @IsNotEmpty()
   authorEmail: string;
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,14 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsBoolean, IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateUserDto {
+  @ApiProperty({ description: 'The email of the user' })
   @IsEmail()
   @IsNotEmpty()
   email: string;
 
+  @ApiProperty({ description: 'The name of the user' })
   @IsString()
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty({ description: 'User is admin', default: false })
   @IsBoolean()
   admin: boolean;
 }
